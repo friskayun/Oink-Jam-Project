c24f175b34350643bf4386a8dd85e7ddefa33724
Friska <friskayunlive@gmail.com>
2026-01-25 17:20:57 +0200

    Enhance Dialogue System
    Added:
    - animated script prompt
    - switching characters based on lines
    - option for item description prompts
    - skip dialogue option

--- project-piggy/UI/Dialogue System/dialogue_system.gd
+++ project-piggy/UI/Dialogue System/dialogue_system.gd
@@ -1,22 +1,40 @@
 extends Control
 
+@onready var timer = $PrintTimer
+
 var dialogues: Dictionary = {}
+
 var dialogue_id: String = ""
 var curr_dialogue
 var line_index: int = 0
+var char_index: int = 0
+
 var active_dialogue: bool = false
+var printing_prompt: bool = false
+
+
 
 func _ready():
 	dialogues = DataManager.read_script_data()
 	hide_dialogue_panel()
+	timer.wait_time = 0.05
 
 func _input(event):
 	## Just for testing / must delete later
 	if event.is_action_pressed("interact") and !active_dialogue:
 		start_dialogue("first_encounter")
+		#start_dialogue("item_description")
 	
 	if event.is_action_pressed("advance_dialogue") and active_dialogue:
 		show_next_line()
+	
+	if event.is_action_pressed("skip_dialogue") and active_dialogue:
+		end_dialogue()
+
+func _on_print_timer_timeout():
+	animate_dialogue_line()
+
+
 
 #region UI functions
 
@@ -26,11 +44,49 @@ func show_dialogue_panel():
 func hide_dialogue_panel():
 	hide()
 
-func change_panel_contents(speaker: String, text: String):
-	%NameLabel.text = speaker
+func show_left_character():
+	%LeftSprite.show()
+	%RightSprite.hide()
+
+func show_right_character():
+	%LeftSprite.hide()
+	%RightSprite.show()
+
+func hide_character_sprites():
+	%LeftSprite.hide()
+	%RightSprite.hide()
+
+func change_panel_contents(text: String, speaker: String  = ""):
+	# switching between character dialogue or item description 
+	if speaker == "":
+		%NameLabel.hide()
+		hide_character_sprites()
+	else:
+		%NameLabel.show()
+		%NameLabel.text = speaker
+		@warning_ignore("standalone_ternary")
+		show_left_character() if speaker == "Player" else show_right_character()
+	
+	# prompt set up
 	%TextLabel.text = text
+	printing_prompt = true
+	timer.start()
+	animate_dialogue_line()
+
+func animate_dialogue_line():
+	char_index += 1
+	%TextLabel.visible_characters = char_index
+	if char_index > %TextLabel.text.length():
+		reset_print_anim()
+
+func reset_print_anim():
+	timer.stop()
+	char_index = 0
+	%TextLabel.visible_characters = -1
+	printing_prompt = false
 
 #endregion
+
 
 func start_dialogue(id: String):
 	line_index = 0
@@ -41,10 +97,18 @@ func start_dialogue(id: String):
 	show_dialogue_panel()
 
 func show_next_line():
+	if printing_prompt:
+		reset_print_anim()
+	
 	if curr_dialogue["lines"].size() > line_index:
 		print("showing next line...")
 		var line = curr_dialogue["lines"][line_index]
-		change_panel_contents(line["speaker"], line["text"])
+		
+		if line.has("speaker"):
+			change_panel_contents(line["text"], line["speaker"])
+		else:
+			change_panel_contents(line["text"])
+		
 		line_index += 1
 	else:
 		print("ending...")

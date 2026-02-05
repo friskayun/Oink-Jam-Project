1d134b022eba650ad1e9144285ee3950e7826c71
Friska <friskayunlive@gmail.com>
2026-02-06 00:49:48 +0200

    Button Focus
    - Added button focus for main menu and pause menu

--- project-piggy/Scenes/UI Scenes/main_menu.gd
+++ project-piggy/Scenes/UI Scenes/main_menu.gd
@@ -3,7 +3,14 @@ extends Control
 const INTRO_CUTSCENE = "intro_cutscene"
 
 func _ready():
-	%LoadButton.disabled = false if DataManager.load_game_data() else true
+	if DataManager.load_game_data():
+		%LoadButton.disabled = false
+		%LoadButton.focus_mode = FOCUS_ALL
+	else:
+		%LoadButton.disabled = true
+		%LoadButton.focus_mode = FOCUS_NONE
+	
+	$VBoxContainer.get_child(0).call_deferred("grab_focus")
 
 func _on_new_button_pressed():
 	GameState._on_new_game()
@@ -11,6 +18,7 @@ func _on_new_button_pressed():
 
 
 func _on_load_button_pressed():
+	
 	GameState._load_checkpoint()
 
 
--- project-piggy/UI/pause_menu.gd
+++ project-piggy/UI/pause_menu.gd
@@ -1,20 +1,34 @@
 extends Control
 
 const INTRO_CUTSCENE = "intro_cutscene"
+@onready var load_button = $VBoxContainer/LoadButton
+@onready var v_box_container = $VBoxContainer
 
 func _ready():
+	setup()
 	hide()
 
 func _input(event):
 	if event.is_action_pressed("skip_dialogue"):
-		if Global.in_cutscene or Global.freeze_input:
+		if Global.in_cutscene:
 			return
 		
 		@warning_ignore("standalone_ternary")
 		hide_panel() if visible else show_panel()
 
+func setup():
+	if DataManager.load_game_data():
+		load_button.disabled = false
+		load_button.focus_mode = FOCUS_ALL
+	else:
+		load_button.disabled = true
+		load_button.focus_mode = FOCUS_NONE
+	
+	v_box_container.get_child(0).call_deferred("grab_focus")
+
 func show_panel():
 	show()
+	setup()
 	get_tree().paused = true
 	Global.freeze_input = true
 
@@ -23,18 +37,23 @@ func hide_panel():
 	get_tree().paused = false
 	Global.freeze_input = false
 
+
+func _on_continue_button_pressed():
+	hide_panel()
+
 func _on_load_button_pressed():
+	hide_panel()
 	GameState._load_checkpoint()
 
-
 func _on_new_button_pressed():
+	hide_panel()
 	GameState._on_new_game()
 	NavigationManager.go_to_level("intro_cutscene")
 
-
 func _on_options_button_pressed():
 	pass # Replace with function body.
-
+	hide_panel()
 
 func _on_exit_button_pressed():
+	hide_panel()
 	NavigationManager.go_to_level("main_menu")
